5de4cef3b790eea4d9c241ba91cd29b46e7dca43
Chris Chou <christopher.chou@publicissapient.com>
2019-08-07 16:58:54 -0500

    Updates

---
+++ precache-manifest.913b4e899517fdb1d5f02d422a389943.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5793e20438ba0956f9a23d30444a332",
+    "url": "/h2a2-react/index.html"
+  },
+  {
+    "revision": "d23c7174553852ccb012",
+    "url": "/h2a2-react/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "f8766f1c9190f447ae2d",
+    "url": "/h2a2-react/static/css/main.2e73ae90.chunk.css"
+  },
+  {
+    "revision": "d23c7174553852ccb012",
+    "url": "/h2a2-react/static/js/2.04101ff3.chunk.js"
+  },
+  {
+    "revision": "f8766f1c9190f447ae2d",
+    "url": "/h2a2-react/static/js/main.212e82b8.chunk.js"
+  },
+  {
+    "revision": "eddcd14c120ed6e3b5e2",
+    "url": "/h2a2-react/static/js/runtime~main.fcd17a74.js"
+  },
+  {
+    "revision": "566ed38b982440ebd4ad1b65575951ad",
+    "url": "/h2a2-react/static/media/cocktail.566ed38b.jpg"
+  },
+  {
+    "revision": "a34402b23c12d801c38c1cfb3fc0d295",
+    "url": "/h2a2-react/static/media/mainst.a34402b2.jpg"
+  },
+  {
+    "revision": "1fd1ac69d9f308b6a87eb4bf0b0d6cba",
+    "url": "/h2a2-react/static/media/todays_background.1fd1ac69.jpg"
+  },
+  {
+    "revision": "a90e533392742ad8b553a74c22f162e2",
+    "url": "/h2a2-react/static/media/welcome1.a90e5333.jpg"
+  }
+]);
